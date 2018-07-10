43a78927d849a5a145f04aee68b8e19b719497c6
RobertWais <wais.robert@uwlax.edu>
2018-07-10 11:18:05 -0700

    Constants added

---
+++ Makestagram/Extensions/Storyboard+Utility.swift
@@ -0,0 +1,9 @@
+//
+//  Storyboard+Utility.swift
+//  Makestagram
+//
+//  Created by Robert Wais on 7/10/18.
+//  Copyright © 2018 Robert Wais. All rights reserved.
+//
+
+import Foundation
---
+++ Makestagram/Supporting Files/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Makestagram
+//
+//  Created by Robert Wais on 7/10/18.
+//  Copyright © 2018 Robert Wais. All rights reserved.
+//
+
+import Foundation
+
+
+struct Constants {
+    struct Segue {
+        static let toCreateUsername = "toCreateUsername"
+    }
+    
+    struct UserDefaults {
+        static let currentUser = "currentUser"
+    }
+}
