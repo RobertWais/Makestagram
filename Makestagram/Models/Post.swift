2b53848215d706d0137d321f078380e5619c1835
RobertWais <wais.robert@uwlax.edu>
2018-07-12 08:39:25 -0700

    Added the property to like a Post in the Post model

--- Makestagram/Models/Post.swift
+++ Makestagram/Models/Post.swift
@@ -10,8 +10,10 @@ import UIKit
 import FirebaseDatabase.FIRDataSnapshot
 class Post {
     
-    
+    let poster: User
+    var likeCount: Int
     var key: String?
+    var isLiked = false
     let imageURL: String
     let imageHeight: CGFloat
     let creationDate: Date
@@ -21,27 +23,39 @@ class Post {
         self.imageURL = imageURL
         self.imageHeight = imageHeight
         self.creationDate = Date()
-        
+        self.likeCount = 0
+        self.poster = User.current
     }
     
     var dictValue: [String: Any] {
         let createdAgo = creationDate.timeIntervalSince1970
         return ["image_url" : imageURL,
                 "image_height" : imageHeight,
-                "created_at" : createdAgo]
+                "created_at" : createdAgo,
+                "like_count" : likeCount,
+                "poster" : poster]
     }
     
     init?(snapshot: DataSnapshot){
         guard let dict = snapshot.value as? [String: Any],
         let imageURL = dict["image_url"] as? String,
         let imageHeight = dict["image_height"] as? CGFloat,
-        let createdAgo = dict["created_at"] as? TimeInterval
+        let createdAgo = dict["created_at"] as? TimeInterval,
+            let likeCount = dict["like_count"] as? Int,
+            let userDict = dict["poster"] as? [String: Any],
+            let uid = userDict["uid"] as? String,
+            let username = userDict["username"] as? String
             else {return nil}
+        
         
         self.key = snapshot.key
         self.imageURL = imageURL
         self.imageHeight = imageHeight
         self.creationDate = Date(timeIntervalSince1970: createdAgo)
+        
+        
+        self.likeCount = 0
+        self.poster = User.current
         
     }
 }
---
+++ Makestagram/Services/LikeService.swift
@@ -0,0 +1,9 @@
+//
+//  LikeService.swift
+//  Makestagram
+//
+//  Created by Robert Wais on 7/11/18.
+//  Copyright © 2018 Robert Wais. All rights reserved.
+//
+
+import Foundation
